9b5f2339166b1e4b68f6c3d72aaa194b4c4bff80
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-07-14 11:59:55 +0200

    style, change date format

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FoodRecipes
+# Card from calendar
+
+Font: 'Fredericka the Great' from https://fonts.google.com,
+Data from API: http://www.vizgr.org/historical-events/,
+Technologies: HTML, SASS, Gulp, jQuery.
--- js/script.js
+++ js/script.js
@@ -2,18 +2,73 @@ $(document).ready(function(){
 
     function loadFacts() {
         $.ajax({
-            url: 'http://www.vizgr.org/historical-events/search.php?begin_date=20010911&end_date=20010912',
+            url: 'http://www.vizgr.org/historical-events/search.php?begin_date=20010911&end_date=20010918',
             method: 'GET'
         }).done(function(response){
             console.log(response);
             $(response).find('event').each(function() {
-                var section = $('#historicalFacts');
-                var date = $(this).find("date").text();
-                var description = $(this).find("description").text();
-                var eventDiv = $('<div>', {class:'event'});
-                var dateDiv = $('<div>', {class:'date'});
-                var infoDiv = $('<div>', {class:'description'});
-                dateDiv.text(date);
+                const section = $('#historicalFacts');
+                const date = $(this).find("date").text().split('/');
+                const year = $('<div>', {class:'year'}).text(date[0]);
+                const month = $('<div>', {class:'month'});
+                switch (date[1]) {
+                    case '01': {
+                        month.text('JANUARY');
+                        break;
+                    }
+                    case '02': {
+                        month.text('FEBRUARY');
+                        break;
+                    }
+                    case '03': {
+                        month.text('MARCH');
+                        break;
+                    }
+                    case '04': {
+                        month.text('APRIL');
+                        break;
+                    }
+                    case '05': {
+                        month.text('MAY');
+                        break;
+                    }
+                    case '06': {
+                        month.text('JUNE');
+                        break;
+                    }
+                    case '07': {
+                        month.text('JULY');
+                        break;
+                    }
+                    case '08': {
+                        month.text('AUGUST');
+                        break;
+                    }
+                    case '09': {
+                        month.text('SEPTEMBER');
+                        break;
+                    }
+                    case '10': {
+                        month.text('OCTOBER');
+                        break;
+                    }
+                    case '11': {
+                        month.text('NOVEMBER');
+                        break;
+                    }
+                    case '12': {
+                        month.text('DECEMBER');
+                        break;
+                    }
+                }
+                const day = $('<div>', {class:'day'}).text(date[2]);
+                const description = $(this).find("description").text();
+                const eventDiv = $('<div>', {class:'event'});
+                const dateDiv = $('<div>', {class:'date'});
+                const infoDiv = $('<div>', {class:'description'});
+                dateDiv.append(year);
+                dateDiv.append(day);
+                dateDiv.append(month);
                 infoDiv.text(description);
                 eventDiv.append(dateDiv);
                 eventDiv.append(infoDiv);
