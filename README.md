51aba7e5f0f4c360fbe0da71688786570d417e02
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-07-15 14:44:59 +0200

    add validation

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Page from calendar
 
-Font: 'Fredericka the Great' from https://fonts.google.com,
-Data from API: http://www.vizgr.org/historical-events/,
-Technologies: HTML, SASS, Gulp, jQuery.
+This web site show historical events / facts as a page from calendar.
+Font: 'Fredericka the Great' from https://fonts.google.com.
+Technologies: HTML, SASS, Gulp, jQuery, AJAX.
+Data are from API: http://www.vizgr.org/historical-events/.
+
+The number of events is 192463:
+German/year: 36883 events from -300/00/00 BC to 2013/12/31
+English/year: 37859 events from -300/00/00 BC to 2012/12/31
+Italian/year: 13813 events from -300/00/00 BC to 2013/11/03
+Spanish/year: 20330 events from -299/00/00 BC to 2013/11/17
+Portuguese/year: 7879 events from -292/00/00 BC to 2013/00/00
+Catalan/year: 7699 events from -300/00/00 BC to 2013/09/11
+Indonesian/year: 2430 events from -300/00/00 BC to 2013/05/10
+Romanian/year: 13936 events from -300/00/00 BC to 2013/07/01
+Turkish/year: 3572 events from 1/00/00 BC to 2013/05/10
+German/month: 9912 events from 2000/00/00 BC to 2012/10/28
+English/month: 38150 events from 2000/00/00 BC to 2012/09/23
 
 https://wojcikkatarzyna.github.io/PageFromCalendar/
--- js/script.js
+++ js/script.js
@@ -7,16 +7,31 @@ $(document).ready(function(){
         let firstDate = '';
         let secondDate = '';
         const begin = $('#firstDate input');
-        for (let i=0; i<begin.length; i++) {
-            firstDate += begin[i].value;
-        }
         const end = $('#secondDate input');
-        for (let i=0; i<end.length; i++) {
-            secondDate += end[i].value;
+        if (begin[0].value > 9999 || end[0].value > 9999) {
+          alert('Sorry, value of year is incorrect.');
+        } else if (begin[0].value > 2013 || end[0].value > 2013) {
+          alert('Please, choose a different period of time. There is no data for perdiod after 2013. ');
+        } else if (begin[1].value < 0 || end[1].value < 0 || begin[1].value > 12 || end[1].value > 12) {
+          alert('Sorry, value of month is incorrect.');
+        } else if (begin[2].value < 0 || end[2].value < 0 || begin[2].value > 31 || end[2].value > 31) {
+          alert('Sorry, value of day is incorrect.');
+        } else if (begin[1].value.split('').length < 2 || end[1].value.split('').length < 2) {
+          alert('Please, add 0 before value of month');
+        } else if (begin[2].value.split('').length < 2 || end[2].value.split('').length < 2) {
+          alert('Please, add 0 before value of day');
+        } else {
+          for (let i=0; i<begin.length; i++) {
+              firstDate += begin[i].value;
+          }
+          for (let i=0; i<end.length; i++) {
+              secondDate += end[i].value;
+          }
+          let urlAPI = 'https://cors-anywhere.herokuapp.com/http://www.vizgr.org/historical-events/search.php?begin_date='+firstDate+'&end_date='+secondDate;
+          console.log(urlAPI);
+          loadFacts(urlAPI);
         }
-        let urlAPI = 'https://cors-anywhere.herokuapp.com/http://www.vizgr.org/historical-events/search.php?begin_date='+firstDate+'&end_date='+secondDate;
-        console.log(urlAPI);
-        loadFacts(urlAPI);
+
     });
 
     function loadFacts(userURL) {
@@ -26,78 +41,80 @@ $(document).ready(function(){
         }).done(function(response){
             section.empty();
             console.log(response);
-            $(response).find('event').each(function() {
-                const date = $(this).find("date").text().split('/');
-                const year = $('<div>', {class:'year'}).text(date[0]);
-                const month = $('<div>', {class:'month'});
-                switch (date[1]) {
-                    case '01': {
-                        month.text('JANUARY');
-                        break;
-                    }
-                    case '02': {
-                        month.text('FEBRUARY');
-                        break;
-                    }
-                    case '03': {
-                        month.text('MARCH');
-                        break;
-                    }
-                    case '04': {
-                        month.text('APRIL');
-                        break;
-                    }
-                    case '05': {
-                        month.text('MAY');
-                        break;
-                    }
-                    case '06': {
-                        month.text('JUNE');
-                        break;
-                    }
-                    case '07': {
-                        month.text('JULY');
-                        break;
-                    }
-                    case '08': {
-                        month.text('AUGUST');
-                        break;
-                    }
-                    case '09': {
-                        month.text('SEPTEMBER');
-                        break;
-                    }
-                    case '10': {
-                        month.text('OCTOBER');
-                        break;
-                    }
-                    case '11': {
-                        month.text('NOVEMBER');
-                        break;
-                    }
-                    case '12': {
-                        month.text('DECEMBER');
-                        break;
-                    }
-                }
-                const day = $('<div>', {class:'day'}).text(date[2]);
-                const description = $(this).find("description").text();
-                const eventDiv = $('<div>', {class:'event'});
-                const dateDiv = $('<div>', {class:'date'});
-                const infoDiv = $('<div>', {class:'description'});
-                dateDiv.append(year);
-                dateDiv.append(day);
-                dateDiv.append(month);
-                infoDiv.text(description);
-                eventDiv.append(dateDiv);
-                eventDiv.append(infoDiv);
-                section.append(eventDiv);
-                console.log($(this));
-            });
+            if (response === 'No events found for this query.') {
+                const noData = $('<div>', {class:'error'}).text('Sorry, there is no events for choosen dates. Try with another period of time.');
+                section.append(noData);
+            } else {
+              $(response).find('event').each(function() {
+                  const date = $(this).find("date").text().split('/');
+                  const year = $('<div>', {class:'year'}).text(date[0]);
+                  const month = $('<div>', {class:'month'});
+                  switch (date[1]) {
+                      case '01': {
+                          month.text('JANUARY');
+                          break;
+                      }
+                      case '02': {
+                          month.text('FEBRUARY');
+                          break;
+                      }
+                      case '03': {
+                          month.text('MARCH');
+                          break;
+                      }
+                      case '04': {
+                          month.text('APRIL');
+                          break;
+                      }
+                      case '05': {
+                          month.text('MAY');
+                          break;
+                      }
+                      case '06': {
+                          month.text('JUNE');
+                          break;
+                      }
+                      case '07': {
+                          month.text('JULY');
+                          break;
+                      }
+                      case '08': {
+                          month.text('AUGUST');
+                          break;
+                      }
+                      case '09': {
+                          month.text('SEPTEMBER');
+                          break;
+                      }
+                      case '10': {
+                          month.text('OCTOBER');
+                          break;
+                      }
+                      case '11': {
+                          month.text('NOVEMBER');
+                          break;
+                      }
+                      case '12': {
+                          month.text('DECEMBER');
+                          break;
+                      }
+                  }
+                  const day = $('<div>', {class:'day'}).text(date[2]);
+                  const description = $(this).find("description").text();
+                  const eventDiv = $('<div>', {class:'event'});
+                  const dateDiv = $('<div>', {class:'date'});
+                  const infoDiv = $('<div>', {class:'description'});
+                  dateDiv.append(year);
+                  dateDiv.append(day);
+                  dateDiv.append(month);
+                  infoDiv.text(description);
+                  eventDiv.append(dateDiv);
+                  eventDiv.append(infoDiv);
+                  section.append(eventDiv);
+              });
+            }
         }).fail(function(error){
-            const errorDiv = $('<div>', {class:'error'});
-            errorDiv.text('Sorry, try with another dates');
-            section.append(errorDiv);
+              console.log(error);
         })
     }
 
